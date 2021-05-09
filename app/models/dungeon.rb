3af5c21dab9d352fbe5bf89806f29e139715bd9d
Mattia Roccoberton <mat@blocknot.es>
2021-05-09 10:49:36 +0200

    Add Dungeon entity
    It will be used to handle a set of rooms and events.
    It has a method to return the introduction message, one to enrich the
    help message when some extra actions are available and another one to
    handle the game inputs.

---
+++ app/models/dungeon.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+class Dungeon
+  attr_accessor :ending
+
+  DIRECTIONS = {
+    "north" => "south",
+    "east" => "west",
+    "south" => "north",
+    "west" => "east"
+  }.freeze
+
+  INVALID_DIRECTION = "You can't go there!"
+
+  def initialize(rooms:, events:)
+    @intro = events["entering"]
+    @ending = ""
+    setup_events(events)
+    setup_rooms(rooms)
+    setup_map(rooms)
+    raise "No rooms defined!" if @rooms.empty?
+  end
+
+  # Enter in the dungeon
+  def entering
+    [@intro, @current_room.describe].compact.join("\n")
+  end
+
+  # Show extra actions help
+  def extra_help_msg
+    actions = @current_room.event&.actions || []
+    msg = actions.map { |key, action| "#{key} - #{action["desc"]}" }.join("\n")
+    "\n#{msg}\n" unless msg.empty?
+  end
+
+  # Handle game commands
+  def handle_action(command)
+    result = @current_room&.event&.handle_action(command)
+    if result
+      result[:msg] << "\n\n#{result[:ending]}" if result[:ending]
+      result
+    elsif DIRECTIONS.key?(command)
+      {msg: change_room(command)}
+    elsif command == "l"
+      {msg: @current_room.describe}
+    end
+  end
+
+  private
+
+  def change_room(dir)
+    if @current_room.near_rooms.include? dir
+      (@current_room = @current_room.near_rooms[dir]).describe
+    else
+      INVALID_DIRECTION
+    end
+  end
+
+  def setup_events(events_data)
+    @events = {}
+    events_data.each do |event, attrs|
+      @events[event] = Event.new(attrs)
+    end
+  end
+
+  def setup_map(rooms_data)
+    rooms_data.each do |room, attrs|
+      DIRECTIONS.each do |dir, rev|
+        next unless attrs[dir]
+
+        near_room = @rooms[attrs[dir]]
+        @rooms[room].link(near_room, dir)
+        # Setup backward link:
+        near_room.link(@rooms[room], rev) unless near_room.final
+      end
+    end
+  end
+
+  def setup_rooms(rooms_data)
+    @rooms = {}
+    rooms_data.each do |room, attrs|
+      event = @events[attrs["event"]]
+      @rooms[room] = Room.new(room, desc: attrs["desc"], event: event, final: attrs["final"])
+    end
+    _, @current_room = @rooms.first
+  end
+end
---
+++ spec/units/dungeon_spec.rb
@@ -0,0 +1,117 @@
+# frozen_string_literal: true
+
+RSpec.describe "Dungeon" do
+  let(:dungeon) { Dungeon.new(rooms: rooms, events: events) }
+  let(:events) { {} }
+  let(:rooms) { {} }
+
+  it "raises an error if there are no rooms" do
+    expect { dungeon }.to raise_exception("No rooms defined!")
+  end
+
+  describe "initialize" do
+    let(:rooms) { {"main_room" => {}} }
+
+    it "set an empty ending" do
+      expect(dungeon.ending).to be_empty
+    end
+
+    context "with some rooms" do
+      let(:rooms) do
+        {
+          "garden" => {
+            "desc" => "There's a gate",
+            "north" => "great_hall"
+          },
+          "great_hall" => {
+            "desc" => "There's a big room"
+          }
+        }
+      end
+
+      before do
+        allow(Room).to receive(:new).and_call_original
+        dungeon
+      end
+
+      it "makes the rooms setup" do
+        expect(Room).to have_received(:new).with("garden", anything).once
+        expect(Room).to have_received(:new).with("great_hall", anything).once
+      end
+    end
+
+    context "with some events" do
+      let(:events) do
+        {
+          "running_rat" => {
+            "intro" => "As soon as you enter a rat run away. Was... he smiling at you?!"
+          },
+          "strange_noise" => {
+            "intro" => "You hear a squeaky sound from your left..."
+          }
+        }
+      end
+
+      before do
+        allow(Event).to receive(:new)
+        dungeon
+      end
+
+      it "makes the events setup" do
+        expect(Event).to have_received(:new).twice
+      end
+    end
+  end
+
+  describe "#entering" do
+    subject(:entering) { dungeon.entering }
+
+    let(:rooms) { {"main_room" => {}} }
+
+    it { is_expected.to start_with "You are in the main room" }
+    it { is_expected.to include "There are no exits" }
+  end
+
+  describe "#extra_help_msg" do
+    subject(:extra_help_msg) { dungeon.extra_help_msg }
+
+    let(:rooms) { {"main_room" => {}} }
+
+    it { is_expected.to be_nil }
+  end
+
+  describe "#handle_action" do
+    subject(:handle_action) { dungeon.handle_action(command) }
+
+    let(:command) { " " }
+    let(:rooms) { {"main_room" => {"desc" => "A nice place"}} }
+
+    it { is_expected.to be_nil }
+
+    context "with command l" do
+      let(:command) { "l" }
+
+      it { is_expected.to match(msg: /There are no exits/) }
+    end
+
+    context "with command north" do
+      let(:command) { "north" }
+
+      it { is_expected.to match(msg: "You can't go there!") }
+    end
+
+    context "with an event" do
+      let(:action) { {done: false, msg: "Some msg"} }
+      let(:room) { instance_double(Room) }
+      let(:event) { instance_double(Event, actions: {"k" => {}}, handle_action: action, state: "a state") }
+      let(:command) { "k" }
+
+      before do
+        allow(Room).to receive(:new).and_return(room)
+        allow(room).to receive(:event).and_return(event)
+      end
+
+      it { is_expected.to eq(done: false, msg: "Some msg") }
+    end
+  end
+end
